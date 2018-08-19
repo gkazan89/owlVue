b9ce9281ddba8ad0683edc4d94f8936a25566d3f
Grant Kazan <grantkazan@gmail.com>
2018-08-19 14:35:31 -0500

    vue grabs basic data from backend

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h2>Preferences</h2>
+    <div v-for="pref in preferences">
+      <p>{{  pref.user_id  }}</p>
+      <p>{{  pref.category_id  }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require("axios");
+export default {
+  data: function() {
+    return {
+      preferences: []
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/preferences").then(
+      function(response) {
+        console.log(response);
+        this.preferences = response.data;
+      }.bind(this)
+    );
+  },
+  methods: {},
+  computed: {}
+};
+</script>
