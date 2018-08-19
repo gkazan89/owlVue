5c5982f64a9029687dfaeaa2b480ddf2f893a9f9
Grant Kazan <grantkazan@gmail.com>
2018-08-19 16:31:25 -0500

    render single user article html

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Articles:</h1>
+    
+    <div v-for="art in articles">
+      <h2>{{  art.title  }}</h2>
+      <h2>{{  art.category  }}</h2>
+      <p>{{  art.author  }}</p>
+      <p>{{  art.body  }}</p>
+    </div>
+
   </div>
 </template>
+
+
+
+
+<script>
+var axios = require("axios");
+export default {
+  data: function() {
+    return {
+      articles: []
+    };
+  },
+  readArticles: function(readArticles) {
+    axios.get("http://localhost:3000/api/articles").then(
+      function(response) {
+        console.log(response);
+        this.articles = response.data;
+      }.bind(this)
+    );
+  },
+
+  methods: {},
+  computed: {}
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="home">
-    <h2>Preferences</h2>
-    <div v-for="pref in preferences">
-      <p>{{  pref.user_id  }}</p>
-      <p>{{  pref.category_id  }}</p>
+    <h2>Articles</h2>
+    <div v-for="art in articles">
+      <h2>{{  art.title  }}</h2>
+      <h3>{{  art.author  }}</h3>
+      <p v-html="art.body"></p>
     </div>
+
   </div>
 </template>
 
@@ -16,7 +18,8 @@ var axios = require("axios");
 export default {
   data: function() {
     return {
-      preferences: []
+      preferences: [],
+      articles: []
     };
   },
   created: function() {
@@ -26,7 +29,15 @@ export default {
         this.preferences = response.data;
       }.bind(this)
     );
+
+    axios.get("http://localhost:3000/api/articles").then(
+      function(response) {
+        console.log(response);
+        this.articles = response.data;
+      }.bind(this)
+    );
   },
+
   methods: {},
   computed: {}
 };
