2f8f7573acd5e353d1df52e79b67c528f802f483
Grant Kazan <grantkazan@gmail.com>
2018-09-02 14:18:21 -0500

    can dynamically build the cube faces

--- src/views/Saph.vue
+++ src/views/Saph.vue
@@ -4,34 +4,19 @@
     <div>
       <!-- Put cube right here -->
       <div class="bigbox">
-        <p class="radio-group" v-on:click="changeSide">
-          <label>
-            <input type="radio" name="rotate-cube-side" value="front" checked /> Technology
-          </label>
-          <label>
-            <input type="radio" name="rotate-cube-side" value="right" /> Sports
-          </label>
-          <label>
-            <input type="radio" name="rotate-cube-side" value="back" /> Music
-          </label>
-          <label>
-            <input type="radio" name="rotate-cube-side" value="left" /> Travel
-          </label>
-          <label>
-            <input type="radio" name="rotate-cube-side" value="top" /> Weather
-          </label>
-          <label>
-            <input type="radio" name="rotate-cube-side" value="bottom" /> US News
-          </label>
-        </p>
+        <!-- fuse the cube with the article text -->
+        <div class="radio-group" v-on:click="changeSide">
+          <div v-for="category in categoryNamesWithSides">
+            <label>
+              <input type="radio" name="rotate-cube-side" v-bind:value="category.side"/> {{category.category}}
+            </label>
+          </div>
+        </div>  
         <div class="scene">
           <div class="cube">
-            <div class="cube__face cube__face--front">Technology</div>
-            <div class="cube__face cube__face--back">Music</div>
-            <div class="cube__face cube__face--right">Sports</div>
-            <div class="cube__face cube__face--left">Travel</div>
-            <div class="cube__face cube__face--top">Weather</div>
-            <div class="cube__face cube__face--bottom">US News</div>
+            <div v-for="category in categoryNamesWithSides">
+              <div v-bind:class="'cube__face cube__face--'+category.side">{{category.category}}</div>
+            </div>
           </div>
         </div>
       </div>  
@@ -39,7 +24,7 @@
       <div v-for="category in categories">
         <h2>{{category.category}}</h2>
         <h4>{{ category.data[category.currentArticleIndex].webTitle }}</h4>
-        <p>{{category.data[category.currentArticleIndex].apiUrl}}</p>
+<!--         <p>{{category.data[category.currentArticleIndex].apiUrl}}</p> -->
         <!-- Hidden article text here -->
         <div>
           <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" v-on:click="upOne(category)">Up One</button>
@@ -57,115 +42,115 @@
           <p v-if="category.currentArticleVisible" v-html="info.response.content.blocks.body[0].bodyHtml"></p>
         </div>
       </div>  
+<!--       <p>{{categories.category.first}}</p> -->
     </div>
   </div>
 </template>
 
 <style>
-  .article_img img {
-    display: block;
-    max-width: 500px;
-    max-height: 300px;
-  }
+.article_img img {
+  display: block;
+  max-width: 500px;
+  max-height: 300px;
+}
 
-  .bigbox {
-    box-sizing: border-box;
-  }
+.bigbox {
+  box-sizing: border-box;
+}
 
-  .scene {
-    width: 500px;
-    height: 500px;
-    border: 1px solid #ccc;
-    margin: 100px;
-    perspective: 600px;
-  }
+.scene {
+  width: 500px;
+  height: 500px;
+  border: 1px solid #ccc;
+  margin: 100px;
+  perspective: 600px;
+}
 
-  .cube {
-    width: 100%;
-    height: 100%;
-    position: relative;
-    transform-style: preserve-3d;
-    transform: translateZ(-250px);
-    transition: transform 1s;
-  }
+.cube {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform-style: preserve-3d;
+  transform: translateZ(-250px);
+  transition: transform 1s;
+}
 
-  .cube.show-front {
-    transform: translateZ(-250px) rotateY(0deg);
-  }
-  .cube.show-right {
-    transform: translateZ(-250px) rotateY(-90deg);
-  }
-  .cube.show-back {
-    transform: translateZ(-250px) rotateY(-180deg);
-  }
-  .cube.show-left {
-    transform: translateZ(-250px) rotateY(90deg);
-  }
-  .cube.show-top {
-    transform: translateZ(-250px) rotateX(-90deg);
-  }
-  .cube.show-bottom {
-    transform: translateZ(-250px) rotateX(90deg);
-  }
+.cube.show-front {
+  transform: translateZ(-250px) rotateY(0deg);
+}
+.cube.show-right {
+  transform: translateZ(-250px) rotateY(-90deg);
+}
+.cube.show-back {
+  transform: translateZ(-250px) rotateY(-180deg);
+}
+.cube.show-left {
+  transform: translateZ(-250px) rotateY(90deg);
+}
+.cube.show-top {
+  transform: translateZ(-250px) rotateX(-90deg);
+}
+.cube.show-bottom {
+  transform: translateZ(-250px) rotateX(90deg);
+}
 
-  .cube__face {
-    position: absolute;
-    width: 500px;
-    height: 500px;
-    border: 2px solid black;
-    line-height: 500px;
-    font-size: 40px;
-    font-weight: bold;
-    color: white;
-    text-align: center;
-  }
+.cube__face {
+  position: absolute;
+  width: 500px;
+  height: 500px;
+  border: 2px solid black;
+  line-height: 500px;
+  font-size: 40px;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+}
 
-  .cube__face--front {
-    background: hsla(0, 100%, 50%, 0.7);
-  }
-  .cube__face--right {
-    background: hsla(60, 100%, 50%, 0.7);
-  }
-  .cube__face--back {
-    background: hsla(120, 100%, 50%, 0.7);
-  }
-  .cube__face--left {
-    background: hsla(180, 100%, 50%, 0.7);
-  }
-  .cube__face--top {
-    background: hsla(240, 100%, 50%, 0.7);
-  }
-  .cube__face--bottom {
-    background: hsla(300, 100%, 50%, 0.7);
-  }
+.cube__face--front {
+  background: hsla(0, 100%, 50%, 0.7);
+}
+.cube__face--right {
+  background: hsla(60, 100%, 50%, 0.7);
+}
+.cube__face--back {
+  background: hsla(120, 100%, 50%, 0.7);
+}
+.cube__face--left {
+  background: hsla(180, 100%, 50%, 0.7);
+}
+.cube__face--top {
+  background: hsla(240, 100%, 50%, 0.7);
+}
+.cube__face--bottom {
+  background: hsla(300, 100%, 50%, 0.7);
+}
 
-  .cube__face--front {
-    transform: rotateY(0deg) translateZ(250px);
-  }
-  .cube__face--right {
-    transform: rotateY(90deg) translateZ(250px);
-  }
-  .cube__face--back {
-    transform: rotateY(180deg) translateZ(250px);
-  }
-  .cube__face--left {
-    transform: rotateY(-90deg) translateZ(250px);
-  }
-  .cube__face--top {
-    transform: rotateX(90deg) translateZ(250px);
-  }
-  .cube__face--bottom {
-    transform: rotateX(-90deg) translateZ(250px);
-  }
+.cube__face--front {
+  transform: rotateY(0deg) translateZ(250px);
+}
+.cube__face--right {
+  transform: rotateY(90deg) translateZ(250px);
+}
+.cube__face--back {
+  transform: rotateY(180deg) translateZ(250px);
+}
+.cube__face--left {
+  transform: rotateY(-90deg) translateZ(250px);
+}
+.cube__face--top {
+  transform: rotateX(90deg) translateZ(250px);
+}
+.cube__face--bottom {
+  transform: rotateX(-90deg) translateZ(250px);
+}
 
-  label {
-    margin-right: 10px;
-  }
+label {
+  margin-right: 10px;
+}
 </style>
 
 
 <script>
-
 // set initial side
 // changeSide();
 
@@ -180,7 +165,8 @@ export default {
       info: [],
       cube: null,
       radioGroup: null,
-      currentClass: ""
+      currentClass: "",
+      categoryNamesWithSides: []
       // can't affect all other categories....
     };
   },
@@ -191,6 +177,18 @@ export default {
         console.log("categories");
         console.log(response);
         this.categories = response.data;
+
+        var sides = ["front", "right", "back", "left", "top", "bottom"];
+        var index = 0;
+        this.categories.forEach(category => {
+          this.categoryNamesWithSides.push({
+            category: category.category,
+            side: sides[index]
+          });
+          index += 1;
+        });
+
+        // this.categoryNamesWithSides = this.categories.map(category => {category: category.category, })
       }.bind(this)
     );
     console.log(this);
