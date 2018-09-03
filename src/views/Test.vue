f7adc7af0eae753af484d42b40acfe37b8891c83
Grant Kazan <grantkazan@gmail.com>
2018-09-03 18:33:34 -0500

    image appears underneath article headline

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -5,7 +5,8 @@
       <div class="reading" v-for="category in categories">
         <h2>{{category.category}} News</h2>
         <h4>{{category.data[category.currentArticleIndex].webTitle}}</h4>
-        <!-- <img v-bind:src="info.response.content.blocks.elements[0].assets[0].file"> WANT IMAGE!!! --> 
+        <img v-if="category.data[category.currentArticleIndex].imageUrl" v-bind:src="category.data[category.currentArticleIndex].imageUrl" alt="">
+        <img v-if="!category.data[category.currentArticleIndex].imageUrl" src="http://thehill.us/wp-content/uploads/2017/05/wait.jpg">
         <div class="buttonNavs">
           <div class="kids">
             <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" v-on:click="downOne(category)">Back</button>
@@ -20,7 +21,7 @@
         <div class="reading" v-if="category.currentArticleVisible">
           <p v-html="info.response.content.blocks.body[0].bodyHtml"></p>
           <div class="collapse">
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" id="show-dialog" v-on:click="read(category)">COLLAPSE PANEL</button>
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" id="show-dialog" v-on:click="read(category)">COLLAPSE ARTICLE</button>
           </div>
         </div>
       </div>  
@@ -73,21 +74,22 @@
           console.log("categories");
           console.log(response);
           this.categories = response.data;
+          this.categories.forEach(category => {
+            console.log("getArticleImage for", category.data[0])
+            this.getArticleImage(category.data[0]);
+          });
           // this.apiUrls = response.data.apiUrl;
-          console.log("HERE ARE YOUR PICS");
-          console.log(this);
+          // console.log("HERE ARE YOUR PICS");
+          // console.log(this);
         }.bind(this)
       );
-
-
-
-      // var other = axios.get();
     },
 
     methods: {
       upOne: function(category) {
         if (category.currentArticleIndex < category.data.length) {
           category.currentArticleIndex += 1;
+          this.getArticleImage(category.data[category.currentArticleIndex]);
         }
         // console.log(category.currentArticleIndex);
       },
@@ -113,6 +115,21 @@
         console.log(category.currentArticleVisible);
         console.log(this);
       },
+      getArticleImage: function(article) {
+        if (!article.imageUrl) {
+          console.log('get the image using ', article.apiUrl);
+          var params = {
+            api_URL: article.apiUrl
+          };
+          // console.log("LOOK AT ME!!!");
+          // console.log(params);
+          axios.get("http://localhost:3000/api/pic/",{params: {api_URL: article.apiUrl}}).then(function(response) {
+            article.imageUrl = response.data.img;
+          }
+          );
+        }
+          // article.imageUrl = "http://wiesmann.codiferes.net/share/bitmaps/test_pattern.svg";
+      }
       // need to figure out how to get this to change
     },
     computed: {}
