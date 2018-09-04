a29b2591c4d240e56be5d7234de297e720568cfe
Grant Kazan <grantkazan@gmail.com>
2018-09-04 14:08:45 -0500

    read article correctly displays scrolling text in cube3.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@
       <router-link to="/logout">Log Out</router-link> |
       <router-link to="/test">Test</router-link> |
       <router-link to="/saph">Cube</router-link> |
-      <router-link to="/cube2">Cube2</router-link>
+      <router-link to="/cube2">Cube2</router-link> |
+      <router-link to="/cube3">Cube3</router-link>
     </div>
     <router-view/>
   </div>
--- src/router.js
+++ src/router.js
@@ -8,18 +8,24 @@ import Logout from "./views/Logout.vue";
 import Test from "./views/Test.vue";
 import Saph from "./views/Saph.vue";
 import Cube2 from "./views/Cube2.vue";
+import Cube3 from "./views/Cube3.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    { path: "/", name: "home", component: Home },
-    { path: "/category", name: "CategorySelect", component: CategorySelect },
-    { path: "/signup", name: "signup", component: Signup },
-    { path: "/login", name: "login", component: Login },
-    { path: "/logout", name: "logout", component: Logout },
-    { path: "/test", name: "test", component: Test },
-    { path: "/saph", name: "saph", component: Saph },
-    { path: "/cube2", name: "cube2", component: Cube2 }
-  ]
+    routes: [
+        { path: "/", name: "home", component: Home },
+        {
+            path: "/category",
+            name: "CategorySelect",
+            component: CategorySelect
+        },
+        { path: "/signup", name: "signup", component: Signup },
+        { path: "/login", name: "login", component: Login },
+        { path: "/logout", name: "logout", component: Logout },
+        { path: "/test", name: "test", component: Test },
+        { path: "/saph", name: "saph", component: Saph },
+        { path: "/cube2", name: "cube2", component: Cube2 },
+        { path: "/cube3", name: "cube3", component: Cube3 }
+    ]
 });
